d50161f156bd9d099378ef662bf96cd7ce8a2e93
shmini923 <45370984+shmini923@users.noreply.github.com>
2019-10-28 11:42:01 -0400

    Add files via upload
    Changed the dataset from the diamonds to the mtcars. Changed the submit button to "Explore"
    The plot will explore mpg vs hp with different car weight

--- app_mod_HS.R
+++ app_mod_HS.R
@@ -2,33 +2,33 @@
 # Shiny app to create a dynamically-filterable visualization of the diamonds app
 ########
 
-# Testing commit
-
 library(shiny)
 library(tidyverse)
 
-min.carat <- min(diamonds$carat)
-max.carat <- max(diamonds$carat)
+head(mtcars)
+
+min.mpg <- min(mtcars$mpg)
+max.mpg <- max(mtcars$mpg)
 
 ui <- fluidPage(
 
-    titlePanel("Diamonds data set viewer"),
+    titlePanel("Fuel efficiency viewer"),
 
     sidebarLayout(
         sidebarPanel(
             
             # This is a range slider (i.e. there's a max and min). It is set that way by "value" (the starting value), which is a 2-element vector
-            sliderInput("carat.adjuster",
-                        "Carats",
-                        min = min.carat,
-                        max = max.carat,
-                        value = c(min.carat, max.carat)),
-            submitButton(text = "Go")
+            sliderInput("mpg.adjuster",
+                        "mpg",
+                        min = min.mpg,
+                        max = max.mpg,
+                        value = c(min.mpg, max.mpg)),
+            submitButton(text = "Explore")
         ),
             
             # Show a plot of diamonds data frame. This output doesn't care what that plot is, only that it will be associated with output$diamonds_plot
         mainPanel(
-           plotOutput("diamonds_plot")
+           plotOutput("mtcars_plot")
         )
     )
 )
@@ -38,16 +38,16 @@ server <- function(input, output) {
     
     d_filt <- reactive({
     
-    low.carat <- input$carat.adjuster[1]
-    hi.carat <- input$carat.adjuster[2]
+    low.mpg <- input$mpg.adjuster[1]
+    hi.mpg <- input$mpg.adjuster[2]
     
-   diamonds %>%
-        filter(carat >= low.carat) %>%
-        filter(carat <= hi.carat)
+   mtcars %>%
+        filter(mpg >= low.mpg) %>%
+        filter(mpg <= hi.mpg)
     })
     
-    output$diamonds_plot <- renderPlot({
-       ggplot(d_filt(), aes_string(x = "carat", y = "y", color = "clarity")) +
+    output$mtcars_plot <- renderPlot({
+       ggplot(d_filt(), aes_string(x = "mpg", y = "hp", color = "wt")) +
                   geom_point()
        })
 }
